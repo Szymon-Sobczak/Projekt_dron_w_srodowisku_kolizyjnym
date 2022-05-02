fb5a7f69161dd81057618239d378c8d5150d5791
Szymon Sobczak <79854057+Szymon-Sobczak@users.noreply.github.com>
2022-05-02 13:02:33 +0200

    projekt3

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ader/Pulpit/DRON_FINAL/zad5_3-Szymon-Sobczak
-# Build directory: /home/ader/Pulpit/DRON_FINAL/zad5_3-Szymon-Sobczak/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
